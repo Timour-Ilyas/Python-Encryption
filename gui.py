2095532efc5c037a148c327025e5307ea1c0ccab
Timour-Ilyas <timour.computer@gmail.com>
2024-01-06 01:38:21 +0100

    Added key Enter box and inserted logic in the window

--- gui.py
+++ gui.py
@@ -5,33 +5,27 @@ from decoder import Decoder
 
 
 class TkView:
-    option = 0
-    secret_message = ""
-    original_message = ""
-    encoder = Encoder()
-    decoder = Decoder()
-
     def __init__(self):
         # setup tkinter
         self.root = tk.Tk()
-        self.root.title("Encrypter")
         self.root.resizable(False, False)
         self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         # create the gui
         self.root.geometry("1280x720")
         self.title_text = tk.StringVar()
-        self.title_text.set("Encrypter")
         self.title_label = tk.Label(self.root, textvariable=self.title_text, font=('Arial', 35))
         self.title_label.pack()
 
         self.first_textbox = tk.Entry(self.root)
         self.first_textbox.pack()
 
-        self.change_mode = tk.Button(self.root, text="Change mode", command=self.change_mode)
-        self.change_mode.pack()
+        self.change_mode_button = tk.Button(self.root, text="Change mode", command=self.change_mode)
+        self.change_mode_button.pack()
 
-        self.second_textbox = tk.Entry(self.root, state="disabled")
+        self.text_of_second_box = tk.StringVar()
+        self.text_of_second_box.set("")
+        self.second_textbox = tk.Entry(self.root, textvariable=self.text_of_second_box, state="disabled")
         self.second_textbox.pack()
 
         self.combobox_mode = ttk.Combobox(self.root, state="readonly")
@@ -39,11 +33,19 @@ class TkView:
         self.combobox_mode.current(0)
         self.combobox_mode.pack()
 
+        self.title_label = tk.Label(self.root, text="Key", font=('Arial', 10))
+        self.title_label.pack()
+        self.key_textbox = tk.Entry(self.root)
+        self.key_textbox.pack()
+
         self.btn_text = tk.StringVar()
-        self.btn_text.set("Encrypt")
-        self.translation_button = tk.Button(self.root, textvariable=self.btn_text, fg="green", bg="black",
-                                            command=self.clicked_main_button)
+        self.translation_button = tk.Button(self.root, textvariable=self.btn_text, command=self.clicked_main_button)
         self.translation_button.pack()
+
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+        self.state_on_encryption = False
+        self.change_mode()
 
     def start_interface(self):
         self.root.mainloop()
@@ -53,21 +55,39 @@ class TkView:
         print("End interface")
 
     def change_mode(self):
-        if self.btn_text.get() == "Encrypt":
+        if self.state_on_encryption:
             self.root.title("Decrypter")
             self.title_text.set("Decrypter")
             self.btn_text.set("Decrypt")
             self.translation_button.config(fg="red", bg="black")
+            self.state_on_encryption = not self.state_on_encryption
         else:
             self.root.title("Encrypter")
             self.title_text.set("Encrypter")
             self.btn_text.set("Encrypt")
             self.translation_button.config(fg="green", bg="black")
+            self.state_on_encryption = not self.state_on_encryption
 
     def clicked_main_button(self):
-        if self.btn_text.get() == "Encrypt":
+        if self.state_on_encryption:
             message = self.first_textbox.get()
-            if True:
+            key = self.key_textbox.get()
+            if not self.combobox_mode.current():
+                secret_message = self.encoder.caesar(message, key)
+                self.text_of_second_box.set(secret_message)
+            if self.combobox_mode.current() == 1:
+                secret_message = self.encoder.vigenere(message, key)
+                self.text_of_second_box.set(secret_message)
+            if self.combobox_mode.current() == 2:
                 pass
         else:
-            pass
+            secret_message = self.first_textbox.get()
+            key = self.key_textbox.get()
+            if not self.combobox_mode.current():
+                message = self.decoder.caesar(secret_message, key)
+                self.text_of_second_box.set(message)
+            if self.combobox_mode.current() == 1:
+                message = self.decoder.vigenere(secret_message, key)
+                self.text_of_second_box.set(message)
+            if self.combobox_mode.current() == 2:
+                pass
