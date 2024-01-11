db3a2dedb631a200cecdfb5a08e149e934d4959c
Timour-Ilyas <timour.computer@gmail.com>
2024-01-11 19:45:49 +0100

    Dynamic console with new encoder attribute

--- console.py
+++ console.py
@@ -6,20 +6,25 @@ def console_using(messages_dict):
     option = 0
     secret_message = ""
     original_message = ""
+    encoder = Encoder()
+    decoder = Decoder()
 
-    while option > 4 or option < 1:
-        option = int(input("Choose the cipher:\n1. Caesar\n2. Vigenère\n3. Symmetric\n4. End program\n"))
-        if option > 4 or option < 1:
+    console_text_options = "Choose the cipher:\n"
+    console_text_options += "".join(str(index) + ". " + value + "\n"
+                                    for index, value in enumerate(encoder.get_encryption_options(), start=1))
+    console_text_options += str(len(encoder.get_encryption_options()) + 1) + ". Exit console\n"
+
+    while option > len(encoder.get_encryption_options()) + 1 or option < 1:
+        option = int(input(console_text_options))
+        if option > len(encoder.get_encryption_options()) + 1 or option < 1:
             print("Incorrect value")
 
-    if option == 4:
+    if option == len(encoder.get_encryption_options()) + 1:
         print("End console")
         return
 
     message = input("Write the message: ")
     key = input("Enter the key: ")
-    encoder = Encoder()
-    decoder = Decoder()
 
     if option == 1:
         secret_message = encoder.caesar(message, key)
@@ -31,6 +36,6 @@ def console_using(messages_dict):
         print("Symmetric cipher")
 
     print(f"{secret_message}")
-    print(original_message)
+    print(f"{original_message}")
 
     messages_dict[secret_message] = original_message
